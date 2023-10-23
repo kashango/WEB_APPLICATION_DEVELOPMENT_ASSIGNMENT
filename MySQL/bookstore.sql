9f114f6da5b378528b24118fb5396676b7869ea4
kashango <112187231+kashango@users.noreply.github.com>
2023-10-23 13:14:04 -0700

    Update bookstore.sql

--- MySQL/bookstore.sql
+++ MySQL/bookstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2023 at 09:12 PM
+-- Generation Time: Oct 23, 2023 at 10:13 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
